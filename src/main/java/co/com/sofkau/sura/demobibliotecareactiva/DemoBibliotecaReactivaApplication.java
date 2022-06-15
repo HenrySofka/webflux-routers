52f199d681638bceabd2c38d04ce5974e146a852
HenrySofka <henry.ferreira@sofka.com.co>
2022-06-15 08:29:08 -0300

    first commit

---
+++ src/main/java/co/com/sofkau/sura/demobibliotecareactiva/DemoBibliotecaReactivaApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.sura.demobibliotecareactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBibliotecaReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBibliotecaReactivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofkau/sura/demobibliotecareactiva/DemoBibliotecaReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.sura.demobibliotecareactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBibliotecaReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
